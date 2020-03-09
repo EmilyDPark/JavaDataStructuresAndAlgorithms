c66a1a37738d795343bd53be7c6910fc4a480c42
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-09 13:38:05 -0700

    MinStack Exercise

--- 1.3 Stacks/MinStack.java
+++ 1.3 Stacks/MinStack.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class MinStack {
     // ------------------------------ Exercise ------------------------------
     // 2 - Design a stack that supports push, pop and retrieving the minimum value in constant time.
@@ -8,5 +10,46 @@ public class MinStack {
     //     stack.pop()
     //     stack.min() // 2
 
+    private int[] items = new int[5];
+    private int count;
+    private int[] min = new int[5];
+    private int minCount;
+
+    public void push(int item) {
+        if (count == items.length)
+            throw new StackOverflowError();
+
+        if (count == 0)
+            min[0] = item;
+        else
+            if (item < min[minCount])
+                min[++minCount] = item;
+
+        items[count++] = item;
+    } // push method
+
+    public int pop() {
+        if (count == 0)
+            throw new IllegalStateException();
+
+        if (items[count - 1] == min[minCount])
+            minCount--;
+
+        return items[--count];
+    } // pop method
+
+    public int min() {
+        return min[minCount];
+    } // min method
+
+    public boolean isEmpty() {
+        return count == 0;
+    } // isEmpty method
+
+    @Override
+    public String toString() {
+        var content = Arrays.copyOfRange(items, 0, count);
+        return Arrays.toString(content);
+    } // toString method
 
 } // MinStack class
