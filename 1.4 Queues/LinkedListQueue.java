db0d7df84ca61e9b79d85816cc7ac62e01195186
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-18 21:30:34 -0700

    Linked List Queue Exericse

---
+++ 1.4 Queues/LinkedListQueue.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class LinkedListQueue {
+    // ------------------------------ Exercise ------------------------------
+    // 2 - Build a queue using a linked list from scratch.
+    //     Implement the following operations and calculate their runtime complexities.
+    //      - enqueue
+    //      - dequeue
+    //      - peek
+    //      - size
+    //      - isEmpty
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+
+    public void enqueue(int item) {
+        var node = new Node(item);
+
+        if (head == null)
+            head = tail = node;
+        else {
+            tail.next = node;
+            tail = node;
+        }
+        count++;
+    }
+
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        int value;
+        if (head == tail) {
+            value = head.value;
+            head = tail = null;
+        }
+        else {
+            value = head.value;
+            var second = head.next;
+            head.next = null;
+            head = second;
+        }
+        count--;
+        return value;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return head.value;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public String toString() {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        Node current = head;
+        while (current != null) {
+            list.add(current.value);
+            current = current.next;
+        }
+
+        return list.toString();
+    }
+
+} // LinkedListQueue class
